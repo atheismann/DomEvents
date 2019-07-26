6c32e5eb6638fa45ccf71c5493654deec45b86d1
myDeveloperJourney <daniel@mydeveloperjourney.com>
2019-07-26 08:30:03 -0500

    adds latest change

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', function(evt){
+    const li = document.createElement('li');
+    const inp = document.querySelector('input');
+    li.textContent = inp.value
+    document.querySelector('ul').appendChild(li);
+    inp.value = "";
+    console.log(li)
+});
