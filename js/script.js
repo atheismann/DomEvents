b30fcf286d7a7522fc3cbd5929160576c9b28964
myDeveloperJourney <daniel@mydeveloperjourney.com>
2019-07-26 09:12:26 -0500

    adds latest changes

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
 const btn = document.querySelector('button');
+const ul = document.querySelector('ul');
+
+
+
 
 btn.addEventListener('click', function(evt){
     const li = document.createElement('li');
     const inp = document.querySelector('input');
-    li.textContent = inp.value
-    document.querySelector('ul').appendChild(li);
-    console.log(li)
+    li.textContent = inp.value;
+    ul.appendChild(li);
+    inp.value = "";
 });
+
+
+
+ul.addEventListener('click', handleClick);
+
+function handleClick(evt) {
+    evt.target.style.fontSize = '40px';
+}
